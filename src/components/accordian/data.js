5f2e86185539eeab320f726915ce66644bcefd0f
fahmy diab <fahmy.diab@vodafone.com>
2024-02-09 20:31:03 +0200

    - accordion react app from FreeCodeCamp

---
+++ src/components/accordian/data.js
@@ -0,0 +1,21 @@
+const data = [
+    {
+        id: '1',
+        question: "What are accordion components?",
+        answer: "Yes of course, it is very possible to create an accordion component with another framework"
+    }, {
+        id: '2',
+        question: "What are they used for?",
+        answer: "Yes of course, it is very possible to create an accordion component with another framework"
+    }, {
+        id: '3',
+        question: "Accordion as a musical instrument",
+        answer: "Yes of course, it is very possible to create an accordion component with another framework"
+    }, {
+        id: '4',
+        question: "Can I create any app component with this framework?",
+        answer: "Yes of course, it is very possible to create an accordion component with another framework"
+    }
+];
+
+export default data;
